dd646a7e69fa764da3f92f7bcef86dbf4c91dc1e
ShinigamiRyuk47 <adityabharambe45@gmail.com>
2024-12-08 17:47:40 -0500

    Update software skills in portfolio.js with new technologies and icons

--- src/portfolio.js
+++ src/portfolio.js
@@ -69,64 +69,51 @@ const skillsSection = {
     emoji(
       "⚡ Optimize robotics systems for real-time applications using modern programming frameworks and languages"
     )
-  ]
-};
+  ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
 https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
       skillName: "python",
       fontAwesomeClassname: "fab fa-python"
     },
     {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
+      skillName: "c-cpp",
+      fontAwesomeClassname: "fab fa-cuttlefish"
+    },
+    {
+      skillName: "solidworks",
+      fontAwesomeClassname: "fas fa-cogs"
+    },
+    {
+      skillName: "autocad",
+      fontAwesomeClassname: "fas fa-drafting-compass"
+    },
+    {
+      skillName: "catia",
+      fontAwesomeClassname: "fas fa-cube"
+    },
+    {
+      skillName: "abaqus",
+      fontAwesomeClassname: "fas fa-chart-bar"
+    },
+    {
+      skillName: "matlab",
+      fontAwesomeClassname: "fas fa-square-root-alt"
+    },
+    {
+      skillName: "ros",
+      fontAwesomeClassname: "fas fa-robot"
+    },
+    {
+      skillName: "webots",
+      fontAwesomeClassname: "fas fa-globe"
+    },
+    {
+      skillName: "additive-manufacturing",
+      fontAwesomeClassname: "fas fa-print"
     }
   ],
   display: true // Set false to hide this section, defaults to true
